c42e885bbd1fe5366638ed3bededd9252d2ce985
FolusoSAMUEL <folusosamuel@gmail.com>
2023-03-08 01:25:48 +0000

    full xapi statement

--- xapi-statement.js
+++ xapi-statement.js
@@ -1,20 +1,27 @@
-function sendStatement(){
+function sendStatement(verb, verbId, object, objectId){
 const player = GetPlayer();
 const uNamejs = player.GetVar("uName");
 const uEmailjs = player.GetVar("uEmail");
+
+const conf ={
+    "endpoint" : "placeholder",
+    "auth" : "Basic " + toBase64("user: password")
+}
+ADL.XAPIWrapper.changeConfig(conf);
+
 const statement = {
-    "actor": {
-"name" : uNamejs,
-"mbox": "mailto:" + uEmailjs
+"actor": {
+        "name" : uNamejs,
+        "mbox": "mailto:" + uEmailjs
     },
 "verb" : {
-"id": "http://adlnet.gov/expapi/verbs/completed",
-"display": {"en-us" : "completed"}
+        "id": verbId,
+        "display": {"en-us" : verb}
 },
 "object": {
-    "id" : "https://www.youtube.com/watch?v=vqWT0xR3JJI",
-    "definition" : {
-        "name": {"en-us" : "Week 2 Xapi cohort"}
+        "id" : objectId,
+        "definition" : {
+            "name": {"en-us" : object}
     }
 }
 }
